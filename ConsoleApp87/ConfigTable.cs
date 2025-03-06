64be7d6acc5505b51fb661525726b33b6f730133
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2025-03-06 16:48:48 +0100

    Add project files.

---
+++ ConsoleApp87/ConfigTable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class Conf
+{
+
+    public Conf(string SqlConnectionString, string a1, string a1 = null, string a3 = null)
+    {
+        var storageAccount = new SqlConnection(SqlConnectionString);
+        storageAccount.Open();
+        string sqlQueryString = $"SELECT * FROM {a1} WHERE region = '{a2}' AND timeSeries_bodyType = '{a3}' AND active = 1;";
+        SqlCommand sqlQuery = new SqlCommand(sqlQueryString, storageAccount);
+        SqlDataReader reader = sqlQuery.ExecuteReader();
+
+    }
+
+}
